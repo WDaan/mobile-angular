46e1656994ba2ef9f8d11270ce05443b9772878f
Daan Wijns <dw.daanwijns@gmail.com>
2020-11-28 12:13:07 +0100

    darkmode for search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { Router } from '@angular/router'
 import { ApiService } from 'src/app/core/api.service'
+import { SettingService } from 'src/app/core/setting.service'
+import { withDarkMode } from 'src/app/mixins/withDarkMode'
 
 @Component({
     selector: 'app-search',
     templateUrl: './search.component.html',
     styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent extends withDarkMode() implements OnInit {
     @Input() value: string
 
-    constructor(private apiService: ApiService, private router: Router) { }
+    constructor(
+        private apiService: ApiService,
+        private router: Router,
+        private settingService: SettingService) {
+        super(settingService)
+    }
 
     ngOnInit(): void {
     }
